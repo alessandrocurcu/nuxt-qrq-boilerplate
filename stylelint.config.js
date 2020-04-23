ced6ee04a24522ac23efcc64b53cb184eba62235
AleQRQ <alessandro.curcu@saep-ict.it>
2020-04-23 15:34:47 +0200

    Aggiunge configurazioni per stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,29 @@
 module.exports = {
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
-  rules: {}
+  extends: [
+    // Use the Standard config as the base
+    // https://github.com/stylelint/stylelint-config-standard
+    'stylelint-config-standard',
+    // Override rules that would interfere with Prettier
+    // https://github.com/shannonmoeller/stylelint-config-prettier
+    'stylelint-config-prettier'
+  ],
+  plugins: [
+    // Bring in some extra rules for SCSS
+    'stylelint-scss'
+  ],
+  rules: {
+    // ===
+    // PRETTIER
+    // ===
+    // HACK: to compensate for https://github.com/shannonmoeller/stylelint-config-prettier/issues/4
+    // Modifying setting from Standard: https://github.com/stylelint/stylelint-config-standard/blob/7b76d7d0060f2e13a331806a09c2096c7536b0a6/index.js#L6
+    'at-rule-empty-line-before': [
+      'always',
+      {
+        except: ['blockless-after-same-name-blockless', 'first-nested'],
+        ignore: ['after-comment'],
+        ignoreAtRules: ['else']
+      }
+    ]
+  }
 }
